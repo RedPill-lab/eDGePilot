dd3e8bceb891efdafc2b047d29a3269db2594a5e
RedPill-lab <155312649+RedPill-lab@users.noreply.github.com>
2025-05-20 01:53:18 +0100

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          strictMode: true
+        }
+      }
+    })
+  ],
   optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    exclude: ['lucide-react']
+  }
 });
