84383b7fcc0b0d2066ba4db9317bd80dd993f331
RedPill-lab <155312649+RedPill-lab@users.noreply.github.com>
2025-05-28 17:24:41 +0100

    Fix Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        parserOpts: {
-          strictMode: true
-        }
-      }
-    })
-  ],
-  optimizeDeps: {
-    exclude: ['lucide-react']
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173
   }
 });
